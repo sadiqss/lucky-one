8251ee49f4fb22ad3b126823161c0941d2c81ee5
sadiqss <sadiq.sau48@gmail.com>
2022-03-26 19:33:26 +0000

    icon added

--- src/component/Countries/Countries.js
+++ src/component/Countries/Countries.js
@@ -3,12 +3,19 @@ import Country from '../Country/Country';
 import './Countries.css';
 
 const Countries = () => {
-    const [countries, setCountries] = useState([])
+    const [countries, setCountries] = useState([]);
+    const [list, setList] = useState([]);
     useEffect(() => {
         fetch('countries.json')
             .then(res => res.json())
             .then(data => setCountries(data))
     }, []);
+
+    const handleAddToList = (country) => {
+        const newList = [...list, country];
+        setList(newList);
+    }
+
     return (
         <div className='travel-container'>
             <div className="countries-container">
@@ -16,11 +23,13 @@ const Countries = () => {
                     countries.map(country => <Country
                         key={country.id}
                         country={country}
+                        handleAddToList={handleAddToList}
                     ></Country>)
                 }
             </div>
             <div className="your-list">
                 <h4>Your list</h4>
+                <p>{list.length}</p>
             </div>
         </div>
     );
--- src/component/Country/Country.js
+++ src/component/Country/Country.js
@@ -1,17 +1,23 @@
+import { faPlane } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Country.css'
 
 const Country = (props) => {
-    const { name, img, continent } = props.country;
+    const { country, handleAddToList } = props;
+    const { name, img, continent, language } = country;
+
     return (
         <div className='country'>
             <img src={img} alt="" />
             <div className='country-info'>
                 <p className='country-name'>Destination:{name}</p>
                 <p>Continent:{continent}</p>
+                <p><small>Language:{language}</small></p>
             </div>
-            <button className='btn-list'>
-                <p>Add to your List</p>
+            <button onClick={() => handleAddToList(country)} className='btn-list'>
+                <p className='btn-text'>Add to your List</p>
+                <FontAwesomeIcon icon={faPlane}></FontAwesomeIcon>
             </button>
         </div>
     );
